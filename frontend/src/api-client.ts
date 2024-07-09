d876b4fe8aeff7403823d16f25362dcb7937a9fb
Saurab Prasai <saurablovetocode@gmail.com>
2024-07-09 20:04:40 +0545

    complete search hotels function api-client

--- frontend/src/api-client.ts
+++ frontend/src/api-client.ts
@@ -1,6 +1,7 @@
 import { LoginFormData } from "./pages/Login";
 import { RegisterFormData } from "./pages/Register";
 import { HotelType } from "../../backend/src/models/hotel";
+import {HotelSearchResponse} from '../../backend/src/routes/hotel'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || "";
 
@@ -109,3 +110,36 @@ export const updateMyHotelById = async (hotelFormData: FormData) => {
 
  return res.json()
 };
+
+
+
+export type SearchParams={
+  destination?:string,
+  checkIn?:Date,
+  checkOut?:Date,
+  adultCount?:number,
+  childCount?:number,
+  page?:string;
+}
+
+
+export const searchHotels=async(searchParams:SearchParams):Promise<HotelSearchResponse>=>{
+    const queryParams=new URLSearchParams();
+    queryParams.append("destination",searchParams.destination||"");
+    queryParams.append("checkIn",searchParams.checkIn||"");
+    queryParams.append("checkOut",searchParams.checkOut||"");
+    queryParams.append("adultCount",searchParams.adultCount||"");
+    queryParams.append("childCount",searchParams.childCount||"");
+    queryParams.append("page",searchParams.page||"");
+
+
+  const response=await fetch(`${API_BASE_URL}/api/hotels/search?${queryParams}`)
+    if(!response.ok){
+      throw new Error("Failed to search hotels")
+    }
+
+    return response.json();
+
+}
+
+
